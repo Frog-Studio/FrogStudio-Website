325ff57a951a128803aee6d613d813279831c99f
Frog Studio <132108888+Frog-Studio@users.noreply.github.com>
2025-12-02 09:12:40 +0100

    az

--- Scripts/hide_header.js
+++ Scripts/hide_header.js
@@ -1,25 +1,28 @@
 var header_hideout = window.innerHeight - 85;
 var header_shown = Date.now();
+const menuToggle = document.getElementsByClassName("menu-toggle")
 
 window.addEventListener("scroll", function() {
   //si 3000ms se sont écoulé & la fenêtre à été scroll sous le landing
-  if (header_shown < Date.now() - 3000 && window.scrollY > header_hideout) {
+  if ((header_shown < Date.now() - 3000) && (window.scrollY > header_hideout)) {
     window.setTimeout(function() {
       document.querySelector("header").style.transform = "translateY(-100%)";
       document.querySelector("header").style.animation = "1s ease-out 0s 0 normal none running none";
     }, 0);
-  } else if (header_shown < Date.now() - 3000 && window.scrollY < header_hideout) {
+  } else if ((header_shown < Date.now() - 3000) && (window.scrollY < header_hideout) && (menuToggle.classList.contains("active"))) {
     document.querySelector("header").style.transform = "translateY(0)";
     document.querySelector("header").style.animation = "1s ease-in 0s 0 normal none running none";
   }
 });
 
+//!menuToggle.classList.contains("active")
+
 document.addEventListener("mousemove", (e) => {
   //si 3000ms se sont écoulé & la fenêtre à été scroll sous le landing & la souris se siture dans la zone du header
-    if (header_shown < Date.now() - 3000 && e.clientY > 100 && window.scrollY > header_hideout) {
+    if ((header_shown < Date.now() - 3000) && (e.clientY > 100) && (window.scrollY > header_hideout)) {
     document.querySelector("header").style.transform = "translateY(-100%)";
     document.querySelector("header").style.animation = "1s ease-out 0s 0 normal none running none";
-  } else if (header_shown < Date.now() - 3000 && e.clientY < 100 && window.scrollY > header_hideout) {
+  } else if ((header_shown < Date.now()) - 3000 && (e.clientY < 100) && (window.scrollY > header_hideout)) {
     document.querySelector("header").style.transform = "translateY(0)";
     document.querySelector("header").style.animation = "1s ease-in 0s 0 normal none running none";
   }
