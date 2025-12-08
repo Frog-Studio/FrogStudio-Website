390c2901dd107fe71592e4f947321f1d572d1eb7
julesdziuba <56728238+julesdziuba@users.noreply.github.com>
2025-12-08 22:49:41 +0100

    script just scripting

--- Scripts/hide_header.js
+++ Scripts/hide_header.js
@@ -1,29 +1,51 @@
 var header_hideout = window.innerHeight - 85;
 var header_shown = Date.now();
-const menuToggle = document.getElementsByClassName("menu-toggle")
+const timerHidding = 3000
 
 window.addEventListener("scroll", function() {
+  //console.log("scrolled")
   //si 3000ms se sont écoulé & la fenêtre à été scroll sous le landing
-  if ((header_shown < Date.now() - 3000) && (window.scrollY > header_hideout)) {
+  if (window.scrollY > header_hideout) {
+    //console.log("scrolled & under hero")
+    if (!menuToggle.classList.contains("active")) {
+      //console.log("active")
+      window.setTimeout(function() {
+        document.querySelector("header").style.transform = "translateY(-100%)";
+        document.querySelector("header").style.animation = "1s ease-out 0s 0 normal none running none";
+        //console.log("Hide header")
+      }, 0);
+    }
+  } else if (window.scrollY < header_hideout) {
     window.setTimeout(function() {
-      document.querySelector("header").style.transform = "translateY(-100%)";
-      document.querySelector("header").style.animation = "1s ease-out 0s 0 normal none running none";
+      document.querySelector("header").style.transform = "translateY(0)";
+      document.querySelector("header").style.animation = "1s ease-in 0s 0 normal none running none";
+      //console.log("Show header")
     }, 0);
-  } else if ((header_shown < Date.now() - 3000) && (window.scrollY < header_hideout) && (menuToggle.classList.contains("active"))) {
-    document.querySelector("header").style.transform = "translateY(0)";
-    document.querySelector("header").style.animation = "1s ease-in 0s 0 normal none running none";
   }
 });
 
 //!menuToggle.classList.contains("active")
 
 document.addEventListener("mousemove", (e) => {
+  //console.log("Mouse moved")
   //si 3000ms se sont écoulé & la fenêtre à été scroll sous le landing & la souris se siture dans la zone du header
-    if ((header_shown < Date.now() - 3000) && (e.clientY > 100) && (window.scrollY > header_hideout)) {
-    document.querySelector("header").style.transform = "translateY(-100%)";
-    document.querySelector("header").style.animation = "1s ease-out 0s 0 normal none running none";
-  } else if ((header_shown < Date.now()) - 3000 && (e.clientY < 100) && (window.scrollY > header_hideout)) {
-    document.querySelector("header").style.transform = "translateY(0)";
-    document.querySelector("header").style.animation = "1s ease-in 0s 0 normal none running none";
+  if (window.scrollY > header_hideout) {  
+    //console.log("Mouse moved & under hero")
+    if (e.clientY > 100) {
+      if (!menuToggle.classList.contains("active")) {
+        //console.log("active")
+        window.setTimeout(function() {
+          document.querySelector("header").style.transform = "translateY(-100%)";
+          document.querySelector("header").style.animation = "1s ease-out 0s 0 normal none running none";
+          //console.log("Hide header")
+        }, 0);
+      }
+    } else if (e.clientY < 100) {
+      window.setTimeout(function() {
+        document.querySelector("header").style.transform = "translateY(0)";
+        document.querySelector("header").style.animation = "1s ease-in 0s 0 normal none running none";
+        //console.log("Show header")
+      }, 0);
+    }
   }
 });
